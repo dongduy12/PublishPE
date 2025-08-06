2cea2804ad330377a9822bf5a23e10e10206ee5b
dongduy12 <dongduy261299@gmail.com>
2025-08-06 17:12:17 +0700

    feat: add API for retrieving data19 by serial number

---
+++ API_WEB/Controllers/SmartFA/RepairTaskDetailController.cs
@@ -0,0 +1,34 @@
+using API_WEB.ModelsOracle;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_WEB.Controllers.SmartFA
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepairTaskDetailController : ControllerBase
+    {
+        private readonly OracleDbContext _oracleContext;
+
+        public RepairTaskDetailController(OracleDbContext oracleContext)
+        {
+            _oracleContext = oracleContext;
+        }
+
+        [HttpGet("data19/{serialNumber}")]
+        public async Task<IActionResult> GetData19BySerial(string serialNumber)
+        {
+            if (string.IsNullOrWhiteSpace(serialNumber))
+            {
+                return BadRequest(new { success = false, message = "Serial number is required." });
+            }
+
+            var data = await _oracleContext.OracleDataRepairTaskDetail
+                .Where(r => r.SERIAL_NUMBER == serialNumber && r.DATA17 != null)
+                .Select(r => r.DATA19)
+                .ToListAsync();
+
+            return Ok(new { success = true, data });
+        }
+    }
+}
